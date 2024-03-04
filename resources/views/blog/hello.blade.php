928490f416fb425000d2a1861f16f78d3a369ba2
Maulana <maulanadwicahyana@gmail.com>
2024-03-04 19:38:54 +0700

    Practiccum 4

--- resources/views/blog/hello.blade.php
+++ resources/views/blog/hello.blade.php
@@ -1,8 +1,24 @@
 <!-- View pada resources/views/hello.blade.php -->
 <html>
+    <head>
+        <title>Data level pengguna</title>
+    </head>
     <body>
         
-        <h1>Hello, {{$name}}</h1>
-        <h1>You are {{ $occupation }}</h1>
+        <h1>Data level pengguna</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Kode Level</th>
+                <th>Nama Level</th>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{ $d->level_id }}</td>
+                    <td>{{ $d->level_kode }}</td>
+                    <td>{{ $d->level_nama }}</td>
+                </tr>
+            @endforeach
+        </table>
     </body>
 </html>
